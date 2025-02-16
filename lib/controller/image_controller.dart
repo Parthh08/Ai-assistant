c8935f6f43ba9cf9189b0d6c69c0e9b6c71e14f8
Parth <www.parthsingh0808@gmail.com>
2025-02-16 17:52:26 +0530

    feat: implement image generation feature with UI and controller

---
+++ lib/controller/image_controller.dart
@@ -0,0 +1,14 @@
+import 'package:ai_assistant/apis/apis.dart';
+import 'package:ai_assistant/model/message.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ImageController extends GetxController {
+  final textC = TextEditingController();
+  Future<void> askQuestion() async {
+    final question = textC.text.trim();
+    if (question.isNotEmpty) {
+      
+    }
+  }
+}
--- lib/screens/feature/image_creater.dart
+++ lib/screens/feature/image_creater.dart
@@ -1,3 +1,5 @@
+import 'package:ai_assistant/controller/image_controller.dart';
+import 'package:ai_assistant/global.dart';
 import 'package:flutter/material.dart';
 
 class ImageGeneratorFeature extends StatefulWidget {
@@ -8,17 +10,66 @@ class ImageGeneratorFeature extends StatefulWidget {
 }
 
 class _ImageGeneratorFeatureState extends State<ImageGeneratorFeature> {
+  final _c = ImageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       appBar: AppBar(
+        backgroundColor: appBarColor,
         title: const Text('Ai Image Generator'),
       ),
-        body: ListView(
-          children: [
-            
-          ],
-        ),
-    );;
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        padding:
+            const EdgeInsets.only(top: 20, bottom: 40, left: 20, right: 20),
+        children: [
+          TextFormField(
+            minLines: 2,
+            maxLines: null,
+            // controller: _c.chatInputController,
+            onTapOutside: (e) => FocusScope.of(context).unfocus(),
+            decoration: InputDecoration(
+              fillColor: Colors.white,
+              filled: true,
+              isDense: true,
+              hintText: 'Generate image from text...',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              suffixIcon: IconButton(
+                onPressed: _c.askQuestion,
+                icon: const Icon(Icons.send),
+              ),
+            ),
+          ),
+
+          //image
+          const SizedBox(height: 20),
+          Image.asset(
+            'assets/images/ai_image_creator.jpg',
+            height: 340,
+          ),
+
+          //generate button
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Generate Image',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    ;
   }
 }
